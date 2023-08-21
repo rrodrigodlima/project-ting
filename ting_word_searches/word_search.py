33bb5f65c406fdd8eaa9703cb62b708695f77695
rrodrigodlima <rdrigo019@gmail.com>
2023-08-21 01:28:52 -0300

    requirements 7 e 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,52 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação """
+from typing import List
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def exists_word(word: str, queue) -> List[dict]:
+    result = []
+
+    for i in range(len(queue)):
+        file_data = queue.search(i)
+        filename = file_data["nome_do_arquivo"]
+        lines_with_word = []
+
+        for line_num, line in enumerate(
+            file_data["linhas_do_arquivo"], start=1
+                ):
+            if word.lower() in line.lower():  # Busca case insensitive
+                lines_with_word.append({"linha": line_num})
+
+        if lines_with_word:
+            result.append({
+                "palavra": word,
+                "arquivo": filename,
+                "ocorrencias": lines_with_word
+            })
+
+    return result
+
+
+def search_by_word(word: str, queue) -> List[dict]:
+    result = []
+
+    for i in range(len(queue)):
+        file_data = queue.search(i)
+        filename = file_data["nome_do_arquivo"]
+        occurrences = []
+
+        for line_num, line in enumerate(
+            file_data["linhas_do_arquivo"], start=1
+                ):
+            if word.lower() in line.lower():  # Busca case insensitive
+                occurrences.append({
+                    "linha": line_num,
+                    "conteudo": line.strip()  # Conteúdo da linha encontrada
+                })
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": filename,
+                "ocorrencias": occurrences
+            })
+
+    return result
